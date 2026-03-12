ce77636c7ae3584325777128bd136f6f00f86aff
Wellington <nylook2@hotmail.com>
2026-03-12 16:34:10 -0300

    :art:Backend tfstate

--- provider.tf
+++ provider.tf
@@ -7,7 +7,16 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "devops-s3-demo-comunidade"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
+
